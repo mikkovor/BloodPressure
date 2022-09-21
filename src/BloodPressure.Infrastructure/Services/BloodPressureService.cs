f4bb331d4c09514af4b13859a2e13f8d930519a6
Mikko Vornanen <mikko.vornanen@wapice.com>
2022-09-21 19:45:04 +0300

    Add service file

--- src/BloodPressure.Infrastructure/Services/BloodPressureService.cs
+++ src/BloodPressure.Infrastructure/Services/BloodPressureService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BloodPressure.Application.Common.Dtos;
+﻿using BloodPressure.Application.Common.Dtos;
 using BloodPressure.Application.Common.Interfaces;
 
 namespace BloodPressure.Infrastructure.Services
